0cfeee895f79ef7a66b8b5f14443a2c2e2b598c7
Rob Foley <trebor641@hotmail.co.uk>
2018-05-03 13:12:02 +0100

    add before_action to reduce redundant code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :show, :update]
+
   def index
     @articles = Article.all
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -23,18 +17,22 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article successfully deleted'
     redirect_to articles_path
   end
 
+  def edit
+  end
+
+  def new
+    @article = Article.new
+  end
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update article_params
       flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
@@ -48,6 +46,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
