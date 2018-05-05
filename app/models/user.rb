02ccd6ee28f292a69ff3f177762a9988d0b781d0
Rob Foley <trebor641@hotmail.co.uk>
2018-05-05 12:29:55 +0100

    Add association between user and articles and debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Why use complex regex when you can use confirmation emails?
   VALID_EMAIL_REGEX = /.+@.+\..+/i
 
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false },
