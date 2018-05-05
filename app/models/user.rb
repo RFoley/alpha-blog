e82e27d7c5d8bacacdcab75b0f10a1eacb79dbeb
Rob Foley <trebor641@hotmail.co.uk>
2018-05-05 11:40:19 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  # Why use complex regex when you can use confirmation emails?
+  VALID_EMAIL_REGEX = /.+@.+\..+/i
 
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
