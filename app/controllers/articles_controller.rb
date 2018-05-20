793cc1bda0aff0a95a49d884fe879df513e4b75a
Rob Foley <trebor641@hotmail.co.uk>
2018-05-20 20:49:29 +0100

    Add authentication and update some small issues, little bit of tidying too

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:destroy, :edit, :show, :update]
+  before_action :set_article, only: %i[destroy edit show update]
+  before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[destroy edit update]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -7,7 +9,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article successfully created'
       redirect_to article_path(@article)
@@ -44,6 +46,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    unless current_user == @article.user
+      flash[:danger] = 'Access denied: You can only edit or delete your own articles'
+      redirect_to root_path
+    end
   end
 
   def set_article
